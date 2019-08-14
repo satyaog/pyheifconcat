c2d8ef67ab391d90da079e8c584c17d858a55b89
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2019-08-14 13:55:32 -0400

    theorical prototype

---
+++ pyheifconcat.py
@@ -0,0 +1,305 @@
+import argparse, collections, glob, importlib.util, os, subprocess, sys
+
+h5py_spec = importlib.util.find_spec("h5py")
+if h5py_spec is not None:
+    import h5py
+
+
+TranscodeArgs = collections.namedtuple("TranscodeArgs",
+                                       ["action", "src", "dest",
+                                        "transcoding_cmd", "ssh_remote", "tmp"])
+
+
+def _get_remote_path(ssh_remote, path):
+    if ssh_remote:
+        return ':'.join([ssh_remote, path])
+    else:
+        return path
+
+
+def _get_completed_list(dest_dir, ssh_remote):
+    completed_list_filepath = os.path.join(dest_dir, "completed_list")
+    process = subprocess.Popen(["rsync",
+                                _get_remote_path(ssh_remote,
+                                                 completed_list_filepath),
+                                '.'])
+    process.wait()
+
+    if os.path.exists("completed_list"):
+        with open("completed_list", 'r') as file:
+            content = file.read()
+
+        completed_list = []
+        for filename in content.split('\n'):
+            if filename.endswith(".transcoded"):
+                filename = filename[0:-len(".transcoded")]
+            completed_list.append(os.path.basename(filename))
+
+        return completed_list
+    else:
+        return []
+
+
+def concat(args):
+    """ Takes a source directory containing files and append/concatenate them
+    into a single destination file
+
+    Files contained in the subdirectory 'queue' of the source directory will be
+    concatenated
+
+    A 'completed_list' containing the concatenated file will be created in the
+    source directory
+
+    Files with a base name that is contained in the 'completed_list' file of
+    the source directory will be ignored
+
+    :param args: parsed arguments
+    """
+    src_dir = args.src
+    dest_dir = os.path.dirname(args.dest)
+    queued_files = glob.glob(os.path.join(src_dir, "queue/", '*'))
+    queued_files.sort()
+
+    if src_dir and not os.path.exists(src_dir):
+        os.makedirs(src_dir)
+    if dest_dir and not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    with open(args.dest, "ab") as concat_file, \
+         open(os.path.join(src_dir, "completed_list"), "a") \
+         as completed_list:
+        for queued_filepath in queued_files:
+            with open(queued_filepath, "rb") as queued_file:
+                concat_file.write(queued_file.read())
+
+            completed_list.write(queued_filepath + '\n')
+            os.remove(queued_filepath)
+
+
+def transcode_img(input_path, dest_dir, args):
+    uploaded_dir = os.path.join(dest_dir, "upload/")
+    queued_dir = os.path.join(dest_dir, "queue/")
+    tmp_dir = args.tmp if args.tmp is not None else \
+              os.path.dirname(input_path)
+    filename = os.path.basename(input_path)
+
+    if uploaded_dir and not os.path.exists(uploaded_dir):
+        os.makedirs(uploaded_dir)
+    if queued_dir and not os.path.exists(queued_dir):
+        os.makedirs(queued_dir)
+    if tmp_dir and not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    output_path = os.path.join(tmp_dir, filename + ".transcoded")
+    command = args.transcoding_cmd.format(src=input_path, dest=output_path)
+    process = subprocess.Popen(command.split())
+    process.wait()
+
+    uploaded_path = os.path.join(uploaded_dir, os.path.basename(output_path))
+    process = subprocess.Popen(["rsync", "--remove-source-files", output_path,
+                                _get_remote_path(args.ssh_remote,
+                                                 uploaded_dir)])
+    process.wait()
+
+    queued_path = os.path.join(queued_dir, os.path.basename(output_path))
+    if args.ssh_remote:
+        subprocess.Popen(["ssh", args.ssh_remote,
+                          "mv {} {}".format(uploaded_path, queued_path)])
+    else:
+        os.rename(uploaded_path, queued_path)
+
+
+def transcode(args):
+    """ Takes a list of images and transcodes them into a destination directory
+
+    The suffix ".transcoded" will be appended to the file a base name
+
+    Subdirectories 'upload' and 'queue' will be created in destination
+    directory where 'upload' contains the files that are being uploaded and
+    'queue' contains the files which are ready to be concatenated
+
+    Files with a base name that is contained in the 'completed_list' file of
+    the destination directory will be ignored
+
+    :param args: parsed arguments
+    """
+    dest_dir = args.dest
+
+    completed_list = _get_completed_list(dest_dir, args.ssh_remote)
+
+    for input_path in args.src.split(','):
+        if os.path.basename(input_path) in completed_list:
+            continue
+        transcode_img(input_path, dest_dir, args)
+
+
+def extract_hdf5(args):
+    """ Takes a source HDF5 file and extracts images from it into a destination
+    directory. If the --transcode parameter is set, images will also be
+    transcoded
+
+    :param args: parsed arguments
+    """
+    if not args.transcode:
+        args.transcoding_cmd = None
+        args.ssh_remote = None
+        args.tmp = None
+
+    tmp_dir = args.tmp if args.tmp is not None else None
+    extract_dir = tmp_dir if tmp_dir is not None else args.dest
+
+    file_h5 = h5py.File(args.src, "r")
+    num_elements = len(file_h5["encoded_images"])
+
+    start = args.start
+    end = min(start + args.number, num_elements) if args.number else num_elements
+
+    if tmp_dir and not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    if extract_dir and not os.path.exists(extract_dir):
+        os.makedirs(extract_dir)
+
+    extracted_filenames = []
+
+    for i in range(start, end):
+        filename = file_h5["filenames"][i][0].decode("utf-8")
+        extract_filepath = os.path.join(extract_dir, filename)
+        extracted_filenames.append(extract_filepath)
+
+        if os.path.exists(extract_filepath):
+            continue
+
+        img_jpeg = bytes(file_h5["encoded_images"][i])
+
+        with open(extract_filepath, "xb") as file:
+            file.write(img_jpeg)
+
+    if args.transcode:
+        transcode_args = TranscodeArgs(action="transcode",
+                                       src=','.join(extracted_filenames),
+                                       dest=args.dest,
+                                       transcoding_cmd=args.transcoding_cmd,
+                                       ssh_remote=args.ssh_remote,
+                                       tmp=tmp_dir)
+
+        transcode(transcode_args)
+
+
+def build_base_parser():
+    parser = argparse.ArgumentParser(description="Benzina HEIF Concatenation",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("action", choices=list(ACTIONS.keys()),
+                        help="action to execute")
+    parser.add_argument('_', metavar="...", nargs=argparse.REMAINDER,
+                        help="use -h {} to view the action's arguments"
+                             .format('{'+','.join(ACTIONS.keys())+'}'))
+
+    return parser
+
+
+def build_concat_parser():
+    parser = argparse.ArgumentParser(description="Benzina HEIF Concatenation action: "
+                                                 "\"concat\"",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("action", metavar="\"concat\"",
+                        help="action to execute")
+    parser.add_argument("src", metavar="source",
+                        help="the source directory containing a 'queue' "
+                             "directory of files to concatenate")
+    parser.add_argument("dest", metavar="destination",
+                        help="the destination concatenated file")
+
+    return parser
+
+
+def build_transcode_parser():
+    parser = argparse.ArgumentParser(description="Benzina HEIF Concatenation action: "
+                                                 "\"transcode\"",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("action", metavar="\"transcode\"",
+                        help="action to execute")
+    parser.add_argument("src", metavar="source",
+                        help="the source file or ',' separated files to transcode")
+    parser.add_argument("dest", metavar="destination",
+                        help="the destination directory for the transcoded file(s)")
+    parser.add_argument("transcoding_cmd", metavar="transcoding-cmd",
+                        help="transcoding command with the following placeholders: "
+                             "{src}, {dest}")
+    parser.add_argument("--ssh-remote", metavar="REMOTE",
+                        help="optional remote to use to transfer the transcoded "
+                             "file to destination")
+    parser.add_argument("--tmp", metavar="DIR",
+                        help="the directory to use to store temporary file(s)")
+
+    return parser
+
+
+def build_extract_hdf5_parser():
+    parser = argparse.ArgumentParser(description="Benzina HEIF Concatenation action: "
+                                                 "\"extract_hdf5\"",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    if extract_hdf5:
+        parser.add_argument("action", metavar="\"extract_hdf5\"",
+                            help="action to execute")
+        parser.add_argument("src", metavar="source",
+                            help="the hdf5 file to draw the images to extract and "
+                                 "transcode if --transcode is True")
+        parser.add_argument("dest", metavar="destination",
+                            help="the destination directory of extracted "
+                                 "file(s) or transcoded file(s) if --transcode "
+                                 "is True")
+        parser.add_argument("--start", default=0, type=int,
+                            help="the start element index to transcode from source")
+        parser.add_argument("--number", default=1000, metavar="NUM", type=int,
+                            help="the number of elements to extract from source")
+
+        parser.add_argument("--transcode", default=False, action="store_true",
+                            help="follow the extraction with a transcoding")
+        parser.add_argument("--transcoding-cmd", metavar="CMD",
+                            help="if --transcode is True, transcoding command "
+                                 "with the following placeholders: "
+                                 "{src}, {dest}")
+        parser.add_argument("--ssh-remote", metavar="REMOTE",
+                            help="if --transcode is True, optional remote to "
+                                 "use to transfer the transcoded file to "
+                                 "destination")
+        parser.add_argument("--tmp", metavar="DIR",
+                            help="if --transcode is True, the directory to "
+                                 "store temporary file(s)")
+
+    else:
+        parser.add_argument("action", choices="\"extract_hdf5\"",
+                            help="Unavailable action as h5py is not accessible")
+
+    return parser
+
+
+def parse_args(raw_arguments=None):
+    argv = sys.argv[1:] if raw_arguments is None else raw_arguments
+    is_help_request = "-h" in argv
+
+    if is_help_request:
+        if len(argv) == 1:
+            build_base_parser().parse_args(argv)
+        argv.remove("-h")
+        base_args = build_base_parser().parse_args(argv)
+        ACTIONS_PARSER.get(base_args.action, None).parse_args(argv + ["-h"])
+
+    base_args = build_base_parser().parse_args(argv)
+    return ACTIONS_PARSER.get(base_args.action, None).parse_args(argv)
+
+
+def main(args):
+    ACTIONS.get(args.action, None)(args)
+
+
+ACTIONS = {"transcode": transcode, "concat": concat, "extract_hdf5": extract_hdf5}
+ACTIONS_PARSER = {"concat": build_concat_parser(),
+                  "transcode": build_transcode_parser(),
+                  "extract_hdf5": build_extract_hdf5_parser(),
+                  "_": build_base_parser()}
+
+
+if __name__ == "__main__":
+    main(parse_args())
---
+++ tests/tests.py
@@ -0,0 +1,222 @@
+import glob, os, shutil, subprocess
+
+from pyheifconcat import concat, transcode, parse_args
+
+
+PWD = "test_tmp"
+
+if PWD and not os.path.exists(PWD):
+    os.makedirs(PWD)
+os.chdir(PWD)
+
+
+def test_concat():
+    src = "input/dir/"
+    dest = "output/dir/concat.bza"
+    src_dir = os.path.dirname(src)
+    dest_dir = os.path.dirname(dest)
+    queued_dir = os.path.join(src_dir, "queue")
+    completed_list_filepath = os.path.join(src_dir, "completed_list")
+
+    to_concat_filepaths = []
+    for i in range(10):
+        to_concat_filepaths.append(os.path.join(queued_dir,
+                                                "file_{}_5mb.img".format(i)))
+
+    args = parse_args(["concat", src, dest])
+
+    try:
+        if queued_dir and not os.path.exists(queued_dir):
+            os.makedirs(queued_dir)
+        if dest_dir and not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        processes = []
+        for i, to_concat_filepath in enumerate(to_concat_filepaths):
+            processes.append(
+                subprocess.Popen(["dd", "if=/dev/urandom",
+                                  "of={}".format(to_concat_filepath),
+                                  "bs=1000", "count=5000"]))
+
+        concat_bytes = []
+        for i, process in enumerate(processes):
+            process.wait()
+            with open(to_concat_filepaths[i], "rb") as file:
+                concat_bytes.append(file.read())
+                assert len(concat_bytes[i]) == 5000 * 1000
+
+        concat_bytes = b''.join(concat_bytes)
+        assert len(concat_bytes) == 5000 * 1000 * 10
+
+        concat(args)
+
+        with open(args.dest, "rb") as file:
+            assert file.read() == concat_bytes
+
+        with open(completed_list_filepath, "r") \
+             as completed_list:
+            assert list(filter(None, completed_list.read().split('\n'))) == \
+                   to_concat_filepaths
+
+    finally:
+        shutil.rmtree(".", ignore_errors=True)
+
+
+def test_concat_no_queue():
+    src = "input/dir/"
+    dest = "output/dir/concat.bza"
+    src_dir = os.path.dirname(src)
+    dest_dir = os.path.dirname(dest)
+    completed_list_filepath = os.path.join(src_dir, "completed_list")
+
+    to_concat_filepaths = []
+
+    args = parse_args(["concat", src, dest])
+
+    try:
+        concat_bytes = b''
+        concat(args)
+
+        with open(args.dest, "rb") as file:
+            assert file.read() == concat_bytes
+
+        with open(completed_list_filepath, "r") \
+             as completed_list:
+            assert list(filter(None, completed_list.read().split('\n'))) == \
+                   to_concat_filepaths
+
+    finally:
+        shutil.rmtree(".", ignore_errors=True)
+
+
+def _test_trancode(tmp_filepaths, nb_files_to_skip, tmp_dir, dest_dir, args):
+    uploaded_dir = os.path.join(dest_dir, "upload")
+    queued_dir = os.path.join(dest_dir, "queue")
+    completed_list_filepath = os.path.join(dest_dir, "completed_list")
+
+    if tmp_dir and not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    if uploaded_dir and not os.path.exists(uploaded_dir):
+        os.makedirs(uploaded_dir)
+    if queued_dir and not os.path.exists(queued_dir):
+        os.makedirs(queued_dir)
+
+    processes = []
+    for i, tmp_filepath in enumerate(tmp_filepaths):
+        processes.append(
+            subprocess.Popen(["dd", "if=/dev/urandom",
+                              "of={}".format(tmp_filepath),
+                              "bs=1000", "count=5000"]))
+
+    files_bytes = []
+    for i, process in enumerate(processes):
+        process.wait()
+        with open(tmp_filepaths[i], "rb") as file:
+            files_bytes.append(file.read())
+            assert len(files_bytes[i]) == 5000 * 1000
+
+    with open(completed_list_filepath, "w") as completed_list_file:
+        for tmp_filepath in tmp_filepaths[:nb_files_to_skip]:
+            tmp_filename = os.path.basename(tmp_filepath)
+            completed_list_file.write(os.path.join(queued_dir, tmp_filename))
+            completed_list_file.write('\n')
+
+    transcode(args)
+
+    assert len(glob.glob(os.path.join(tmp_dir, '*'))) == nb_files_to_skip
+    assert len(glob.glob(os.path.join(uploaded_dir, '*'))) == 0
+
+    queued_list = glob.glob(os.path.join(queued_dir, '*'))
+    queued_list.sort()
+    assert len(queued_list) == \
+           len(tmp_filepaths) - nb_files_to_skip
+
+    for i, filepath in enumerate(queued_list):
+        with open(filepath, "rb") as file:
+            file_bytes = file.read()
+            assert file_bytes == files_bytes[i + nb_files_to_skip]
+
+
+def test_trancode():
+    dest = "output/dir/"
+    dest_dir = os.path.dirname(dest)
+    tmp_dir = "tmp_test"
+
+    tmp_filepaths = []
+    for i in range(10):
+        tmp_filepaths.append(os.path.join(tmp_dir, "file_{}_5mb.img".format(i)))
+
+    args = parse_args(["transcode", ','.join(tmp_filepaths), dest,
+                       "mv {src} {dest}"])
+
+    try:
+        _test_trancode(tmp_filepaths, 0, tmp_dir, dest_dir, args)
+
+    finally:
+        shutil.rmtree(".", ignore_errors=True)
+
+
+def test_trancode_completed_3():
+    dest = "output/dir/"
+    dest_dir = os.path.dirname(dest)
+    tmp_dir = "tmp_test"
+
+    tmp_filepaths = []
+    for i in range(10):
+        tmp_filepaths.append(os.path.join(tmp_dir, "file_{}_5mb.img".format(i)))
+
+    args = parse_args(["transcode", ','.join(tmp_filepaths), dest,
+                       "mv {src} {dest}"])
+
+    try:
+        _test_trancode(tmp_filepaths, 3, tmp_dir, dest_dir, args)
+
+    finally:
+        shutil.rmtree(".", ignore_errors=True)
+
+
+def test_action_redirection():
+    raw_concat_arguments = ["concat", "src_concat", "dest_concat"]
+
+    raw_transcode_arguments = ["transcode",
+                               "src_transcode",
+                               "dest_transcode",
+                               "transcoding_cmd_transcode",
+                               "--ssh-remote", "remote_transcode",
+                               "--tmp", "tmp_transcode"]
+
+    raw_extract_hdf5_arguments = ["extract_hdf5",
+                                  "src_extract_hdf5",
+                                  "dest_extract_hdf5",
+                                  "--start", "10",
+                                  "--number", "15",
+                                  "--transcode",
+                                  "--transcoding-cmd", "transcoding_cmd_extract_hdf5",
+                                  "--ssh-remote", "remote_extract_hdf5",
+                                  "--tmp", "tmp_extract_hdf5"]
+
+    args = parse_args(raw_concat_arguments)
+
+    assert args.action == "concat"
+    assert args.src == "src_concat"
+    assert args.dest == "dest_concat"
+
+    args = parse_args(raw_transcode_arguments)
+
+    assert args.action == "transcode"
+    assert args.src == "src_transcode"
+    assert args.dest == "dest_transcode"
+    assert args.transcoding_cmd == "transcoding_cmd_transcode"
+    assert args.ssh_remote == "remote_transcode"
+    assert args.tmp == "tmp_transcode"
+
+    args = parse_args(raw_extract_hdf5_arguments)
+
+    assert args.action == "extract_hdf5"
+    assert args.src == "src_extract_hdf5"
+    assert args.dest == "dest_extract_hdf5"
+    assert args.start == 10
+    assert args.number == 15
+    assert args.transcoding_cmd == "transcoding_cmd_extract_hdf5"
+    assert args.ssh_remote == "remote_extract_hdf5"
+    assert args.tmp == "tmp_extract_hdf5"
